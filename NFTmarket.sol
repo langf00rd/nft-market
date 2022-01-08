4425601845b7e5d0722b1cc63e380cff22d18574
Langford Kwabena <55645613+langfordquarshie21@users.noreply.github.com>
2022-01-08 18:31:06 +0000

    Update NFTmarket.sol

--- NFTmarket.sol
+++ NFTmarket.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.2;
-pragma experimental ABIEncoderV2;
 
 import "./IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
